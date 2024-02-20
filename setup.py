601ae52df5a9df73eb6b014a123f46c3e08830e5
Daniela Rus Morales <danirus@eml.cc>
2024-02-20 16:48:32 +0100

    Fix README, prepare rel 0.2.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(BASE_DIR / "requirements.txt", "r") as req_file:
 
 setup(
     name="sphinx-nefertiti",
-    version="0.2.2",
+    version="0.2.3",
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
