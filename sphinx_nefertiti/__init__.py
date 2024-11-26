0ea8ea82af3e8d20191df3923b3b3a35a9c23d4e
Daniela Rus Morales <157103+danirus@users.noreply.github.com>
2024-11-26 16:43:08 +0100

    Prepare release 0.5.2 (#44)

--- sphinx_nefertiti/__init__.py
+++ sphinx_nefertiti/__init__.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 from sphinx_nefertiti import colorsets, docsver, fonts, links, pygments
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 pages_wo_index = ["genindex", "search"]
 
